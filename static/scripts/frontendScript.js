76f35a7bf1c7d69cd0209ca8036c83dbd97faab5
constantin <constantindietrich@hotmail.de>
2025-03-26 20:35:29 +0100

    Add dynamic canvas workshop floor rendering

--- static/scripts/frontendScript.js
+++ static/scripts/frontendScript.js
@@ -10,49 +10,23 @@ c.height = rect.height * dpr; // Set actual height
 
 ctx.scale(dpr, dpr); // Scale the context
 
-// Set the canvas background color
-ctx.fillStyle = "#1e1e1e";
-ctx.fillRect(0, 0, c.width, c.height);
-
-// Adjust coordinates for sharp lines
-
-// Create a Grid for the Warehouse
-//let gridSize = Math.floor(c.height / 21);
-
-/*
-let numGridsHeight = Math.floor(c.height / gridSize);
-let numGridsWidth = Math.floor(c.width / gridSize);
-for (let i = 0; i < c.width; i += gridSize) {
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, c.height);
-    ctx.stroke();
-    ctx.moveTo(0, i);
-    ctx.lineTo(c.width, i);
-    ctx.stroke();
-
-}*/
-//drawPackingTable(gridSize, c.height, c.width);
-//drawStorageShelfRow(gridSize, c.height - gridSize * 7, gridSize, gridSize * 6, 5);
-//drawStorageShelfRow(gridSize, c.height - gridSize * 14, gridSize, gridSize * 6, 5);
-
-
 // Create the Packing Table (Rectangle)
-function drawPackingTable(gridSize, screenHeight, screenWidth) {
-    let rectX = (((screenWidth / gridSize) / 2) - 1) * gridSize;
-    let rectWidth = gridSize * 2;
-    ctx.rect(rectX, gridSize, rectWidth, gridSize); // x, y, width, height
+function drawPackingTable(numAisles, horizontalGridSize, verticalGridSize, screenHeight, screenWidth) {
+    let positionX = (screenWidth / 2) - ((numAisles * 4 * horizontalGridSize) / 2);
+
+    ctx.rect(positionX, verticalGridSize, horizontalGridSize * 3, verticalGridSize); // x, y, width, height
     ctx.fillStyle = "lightblue";
     ctx.fill(); // Draw the outline
 
 // Set text styles
     ctx.font = "15px Arial";
     ctx.fillStyle = "black";
-    ctx.textAlign = "center"; // Horizontal alignment
-    ctx.textBaseline = "middle"; // Vertical alignment
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
 
 // Calculate text position (center of the rectangle)
-    let textX = rectX + rectWidth / 2; // Corrected calculation
-    let textY = gridSize + gridSize / 2;
+    let textX = positionX + (horizontalGridSize * 3) / 2;
+    let textY = verticalGridSize + verticalGridSize / 2;
 
     ctx.fillText("Packing Table", textX, textY);
 }
@@ -61,7 +35,7 @@ function drawStorageShelf(positionX, positionY, horizontalGridSize, shelfHeight)
     ctx.beginPath();
     ctx.rect(positionX, positionY, 2 * horizontalGridSize, shelfHeight); // x, y, width, height
     ctx.fillStyle = "lightblue";
-    ctx.fill(); // Draw the outline
+    ctx.fill();
 
 }
 
@@ -71,24 +45,25 @@ function drawStorageShelfRow(startPositionY, horizontalGridSize, shelfHeight, nu
 
     for (let i = 0; i < numAisles; i++) {
         drawStorageShelf(startPosition, startPositionY, horizontalGridSize, shelfHeight);
-        startPosition = startPosition + 5 * horizontalGridSize;
+        startPosition = startPosition + 4 * horizontalGridSize;
     }
 }
 
 function drawStorageShelfFloorPlan(numAisles, numCrossing, screenHeight, screenWidth) {
-    let marginTop = 3;
-    let marginBottom = 2;
+    // Set the canvas background color
+    ctx.fillStyle = "#1e1e1e";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    let marginVertical = 20;
+    let marginHorizontal = 20;
 
     // the grid sizes are dynamically calculated based on the number of aisles and the screen height
-    let verticalGridSizeAmount = Math.floor(screenHeight / ((numCrossing * 7) + marginBottom + marginTop));
-    let horizontalGridSizeAmount = Math.floor(screenWidth / (numAisles * 4));
+    let verticalGridSizeAmount = Math.floor(screenHeight / ((numCrossing * 7) + marginVertical));
+    let horizontalGridSizeAmount = Math.floor(screenWidth / (numAisles * 4 + marginHorizontal));
 
-    //let shelfHeight = Math.floor(screenHeight / ((numCrossing * 7) + marginTop + marginBottom));
-    //let aisleSpacing = Math.floor(screenWidth / (numAisles * 6)); // Ensure spacing shrinks as aisles increase
-    //let shelfWidth = aisleSpacing * 2;
 
-    let verticalGridSize = Math.floor(screenHeight / verticalGridSizeAmount);
-    let horizontalGridSize = Math.floor(screenWidth / horizontalGridSizeAmount);
+    let verticalGridSize = verticalGridSizeAmount // Math.floor(screenHeight / verticalGridSizeAmount);
+    let horizontalGridSize = horizontalGridSizeAmount // Math.floor(screenWidth / horizontalGridSizeAmount);
 
     let offset = (numCrossing * 7 * verticalGridSize) / 2;
     let startPositionY = (screenHeight / 2) + offset;
@@ -97,8 +72,22 @@ function drawStorageShelfFloorPlan(numAisles, numCrossing, screenHeight, screenW
         drawStorageShelfRow(startPositionY, horizontalGridSize, verticalGridSize * 6, numAisles, screenWidth);
         startPositionY -= verticalGridSize * 7;
     }
+
+    // Todo get the packing table to the right position
+    drawPackingTable(numAisles, horizontalGridSize, verticalGridSize, screenHeight, screenWidth)
 }
 
 drawStorageShelfFloorPlan(4, 3, c.height, c.width);
 
+document.getElementById("changeWarehouseFloorPlan").addEventListener("click", function () {
+    let aisles = document.getElementById("warehouseAisleCount").value;
+    let crossings = document.getElementById("warehouseCrossingCount").value;
 
+    if (aisles < 1 || crossings < 1) {
+        alert("Please enter a valid number for aisles and crossings");
+        return;
+    }
+
+    ctx.clearRect(0, 0, c.width, c.height);
+    drawStorageShelfFloorPlan(aisles, crossings, c.height, c.width);
+});
