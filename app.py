add2b0f3f6e6c2bcbd48ed88554d5ad77f255502
constantin <constantindietrich@hotmail.de>
2025-03-29 00:44:11 +0100

    add test stock location set generation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
@@ -7,11 +8,28 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('index.html')
 
-@app.route('/generate-picking', methods=['POST'])
-def generate_picking():
-    # todo decide if this should be triggered over js with context
-    product_count = request.form.get('product_count')  # Get data from form submission
-    return f"Processing {product_count} products"
+@app.route('/generate-test-locations', methods=['POST'])
+def generate_test_locations() -> [(int, int)]:
+    """Computes the number of locations and generates random locations"""
+
+    info = request.get_json()
+
+    number_of_shelf_columns = info.get('numAisles') # todo fix this naming issue
+    number_of_rows = info.get('numCrossings')
+    product_count = info.get('product_count')
+
+    # As each shelf is 6 grids high and 2 grids wide we assume that shelf has 12 stock locations
+    number_of_shelves = number_of_rows * number_of_shelf_columns
+    number_of_locations = number_of_shelves * 12
+
+    locations = [random.randint(0, number_of_locations) for _ in range(product_count)]
+
+    return
+    # Todo letzter status: versucht die locations in die grid tuples zu übertragen. Hierfür dachte ich daran im
+    # frontend die obere ecke des squares für die berechnung zu nehmen. Die Koordinate der oberen ecke ist
+    # (wareHouseFloorPlan.canvas.height / 2) + wareHouseFloorPlan.offset + die höhe der anderen regale und lücken
+
+    # Todo: Überlege welche varialblen im frontend wirklich this sein müssen und welche nicht.
 
 
 
--- static/scripts/frontendScript.js
+++ static/scripts/frontendScript.js
@@ -44,15 +44,6 @@ class WarehouseFloorPlan {
         this.ctx.fillText("Packing Table", textX, textY);
     }
 
-    drawPickingMarker(positionX, positionY, horizontalGridSize, verticalGridSize) {
-        // draws a small red dot to indicate the picking location
-        this.ctx.beginPath();
-        this.ctx.arc(positionX, positionY, Math.max(horizontalGridSize, verticalGridSize) / 5, 0, 2 * Math.PI);
-        this.ctx.stroke();
-        this.ctx.fillStyle = "red";
-        this.ctx.fill();
-    }
-
     drawStorageShelf(positionX, positionY) {
         this.ctx.beginPath();
         this.ctx.rect(positionX, positionY, 2 * this.horizontalGridSize, this.shelfHeight * this.verticalGridSize); // x, y, width, height
@@ -70,6 +61,31 @@ class WarehouseFloorPlan {
         }
     }
 
+    drawPickingMarker(positionX, positionY) {
+        // draws a small red dot to indicate the picking location
+        this.ctx.beginPath();
+        this.ctx.arc(positionX, positionY, Math.max(this.horizontalGridSize, this.verticalGridSize) / 12, 0, 2 * Math.PI);
+        this.ctx.stroke();
+        this.ctx.fillStyle = "red";
+        this.ctx.fill();
+    }
+
+    drawWarehouseRoute(route) {
+        // draw the route that is given in tuples from the backend
+        //      - valid aisles are multiples of xStart + or -4 as one shelf is 2 grids wide
+        //      - valid columns are multiples of xStart + 0,5 offset and + or minus 6
+
+
+        this.ctx.beginPath();
+        for (let i = 0; i < route.length; i++) {
+            this.ctx.lineTo(route[i].x * this.horizontalGridSize, route[i].y * this.verticalGridSize);
+            this.ctx.strokeStyle = "red";
+        }
+        this.ctx.strokeStyle = "red"; // Set the line color to red
+        this.ctx.lineWidth = 3;
+        this.ctx.stroke();
+    }
+
     drawStorageShelfFloorPlan(numAisles, numCrossings) {
         this.clearCanvas();
         this.numAisles = numAisles;
@@ -81,27 +97,32 @@ class WarehouseFloorPlan {
         this.shelfHeight = 6;
 
         // the grid sizes are dynamically calculated based on the number of aisles and the screen height
-        this.verticalGridSize = Math.floor(this.canvas.height / ((numCrossings * 7) + this.marginVertical));
-        this.horizontalGridSize = Math.floor(this.canvas.width / (numAisles * 4 + this.marginHorizontal));
+        this.verticalGridSize = this.canvas.height / ((numCrossings * 7) + this.marginVertical);
+        this.horizontalGridSize = this.canvas.width / (numAisles * 4 + this.marginHorizontal);
 
         this.offset = (numCrossings * 7 * this.verticalGridSize) / 2;
         this.startPositionY = (this.canvas.height / 2) + this.offset;
 
         for (let i = 0; i < numCrossings; i++) {
-            this.drawStorageShelfRow(this.startPositionY, this.horizontalGridSize, numAisles);
+            this.drawStorageShelfRow();
             this.startPositionY -= this.verticalGridSize * 7;
         }
 
         // draw the packing table that is the start and end point of the route
         this.drawPackingTable();
-        this.drawPickingMarker();
     }
 }
 
-const wareHouseFloorPlan = new WarehouseFloorPlan("warehouseCanvas");
-wareHouseFloorPlan.drawStorageShelfFloorPlan(4, 3, wareHouseFloorPlan.canvas.height, wareHouseFloorPlan.canvas.width);
 
-document.getElementById("changeWarehouseFloorPlan").addEventListener("click", function () {
+const wareHouseFloorPlan = new WarehouseFloorPlan("warehouseCanvas");
+const changeWarehouseFloorPlanButton = document.getElementById("changeWarehouseFloorPlan");
+const generateLocationTestSetButton = document.getElementById("generateLocationsButton");
+
+wareHouseFloorPlan.drawStorageShelfFloorPlan(4, 3, wareHouseFloorPlan.canvas.height, wareHouseFloorPlan.canvas.width);
+wareHouseFloorPlan.drawPickingMarker(20, (wareHouseFloorPlan.canvas.height / 2) + wareHouseFloorPlan.offset);
+wareHouseFloorPlan.drawWarehouseRoute([{x: 0, y: 30.5}, {x: 50, y: 30.5}]);
+
+changeWarehouseFloorPlanButton.addEventListener("click", function () {
     let aisles = document.getElementById("warehouseAisleCount").value;
     let crossings = document.getElementById("warehouseCrossingCount").value;
 
@@ -111,25 +132,22 @@ document.getElementById("changeWarehouseFloorPlan").addEventListener("click", fu
     }
     wareHouseFloorPlan.drawStorageShelfFloorPlan(aisles, crossings);
 });
-/*
-document.getElementById("generateLocationTestSet").addEventListener("click", function() {
-    let productCount = document.getElementById("generateLocationTestSet").value;
+
+
+generateLocationTestSetButton.addEventListener("click", function () {
+    let productCount = document.getElementById("locationCount").value;
 
     fetch('/generate-test-locations', {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ product_count: productCount,
-                               warehouse_aisles: document.getElementById("warehouseAisleCount").value,
-                               warehouse_crossings: document.getElementById("warehouseCrossingCount").value,
-                               warehouse_width: })
+        body: JSON.stringify({warehouse_floor_plan: wareHouseFloorPlan, product_count: productCount})
     })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.message);
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
+        })
+        .catch(error => console.error('Error:', error));
 });
 
-*/
